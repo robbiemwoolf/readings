6f3d1322b9cc6743c99e45bb59f15f1e47fec746
Robbie Woolf <robbiemwoolf@gmail.com>
2022-07-19 08:56:22 -0700

    33.2 complete

---
+++ src/backend/postgres-33/creating-deleting-dbs-2.js
@@ -0,0 +1,21 @@
+// Postgres 33
+
+    // overview: Databases are everywhere, and they're invaluable for storing persistent information. So in this module, you'll learn the basics of working with databases.
+
+// Creating and Deleting Databases 33.2
+
+    // overview: Using a hosted database will let you quickly get set up with a PostgreSQL database, regardless of your operating system and your computer's performance characteristics. By the end of this lesson, you will be able to create and delete a remote postgreSQL database.
+
+// terms: 
+
+    // hosted database: also called a managed database, a cloud-computing service in which the end user pays a cloud service provider for access to a database
+
+// Hosted Databases
+
+    // Unlike a local database, you don't have to set up or maintain a hosted database on your own; rather, it's the provider's responsibility to oversee the database's infrastructure. This allows you to focus on building your application instead of spending time installing, configuring, and updating your database.
+
+// Creating and Deleting Databases
+
+    // The free databases hosted on ElephantSQL cost the company a little bit of money for every second that each database exists. To ensure that the free database service continues to be free, you should promptly delete your database as soon as you are done with it. Even if you are only going to stop using the database for a short amount of time, it is best to extract the data, delete the database, and then create a new database later when you need it.
+
+// use provided info from elephantSQL to reference your db in js
