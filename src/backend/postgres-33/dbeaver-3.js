b71fde3bf604de139e36299d90c95047f985331d
Robbie Woolf <robbiemwoolf@gmail.com>
2022-07-19 09:32:54 -0700

    moudle 33.3 complete

---
+++ src/backend/postgres-33/dbeaver-3.js
@@ -0,0 +1,15 @@
+// Postgres 33
+
+// Installing dBeaver 33.3
+
+    // overview: The easiest way to manage, view, and update a database hosted on the internet is to install a graphical user interface client. Once you install a GUI (pronounced like gooey) client in this lesson, you'll learn how to use it to mange almost every aspect of the hosted database.
+
+// terms:
+
+    // structured query language: SQL, a programming language used to manipulate and query data in relational databases
+
+// DBeaver
+
+    // DBeaver is a free multiplatform database tool for developers, SQL programmers, database administrators, and analysts. It essentially supports any database.
+
+// It is possible to use the DBeaver user interface to create the users table, but this module will use SQL exclusively. Using SQL not only means that you can commit the SQL to Git and have the normal advantages of source code control; it also means that you can automate database-management tasks using SQL rather than having to run them manually through a UI.
